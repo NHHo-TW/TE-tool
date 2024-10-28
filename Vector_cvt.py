5f9d5ac278f30980a0696c0a35bdf786b368f3e8
NHHo-TW <moonnight1109@gmail.com>
2024-10-28 20:14:01 +0800

    Ver 1.0.1

--- Vector_cvt.py
+++ Vector_cvt.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Ver 1.0.1
-程式整合簡化 （FORMAT + ASSIGN 的部分）
+程式整合簡化 (FORMAT + ASSIGN 的部分）
 
 Ver 1.0.0
 Beta, 支援 tstl2 檔案
@@ -86,7 +86,7 @@ def tstl2_vector(file_source_path, file_target_path, DPINLIST, LCDPINLIST):
                     except:
                         logger.error(f"There is new rule in {file_name}, contact author pls")
                     if LINE == 1: OutStr = "{:<10}{:<15}{}{}{:<5}{:<10} \n" .format("ST:", "NOP", TN, pattern, " ", str(LINE))
-                    elif rpt_cnt: OutStr = "{:<10}{:<15}{}{}{:<5}{:<10} \n" .format(" ", "IDXI " + str(rpt_cnt), TN, pattern, " ", str(LINE) + " Repeat " + str(rpt_cnt + 1))
+                    elif rpt_cnt: OutStr = "{:<10}{:<15}{}{}{:<5}{:<10} \n" .format(" "  , "IDXI " + str(rpt_cnt), TN, pattern, " ", str(LINE) + " Repeat " + str(rpt_cnt + 1))
                     else        : OutStr = "{:<10}{:<15}{}{}{:<5}{:<10} \n" .format(" "  , "NOP", TN, pattern, " ", str(LINE))
                     if int(numcheck) != LINE:
                         new_rule += 1 #should add warning log
@@ -98,7 +98,85 @@ def tstl2_vector(file_source_path, file_target_path, DPINLIST, LCDPINLIST):
     if new_rule:
         logger.warning(f"There is new rule in {file_name}, contact author pls")
 
+def vector_trans(file_source_path, DPINLIST, LCDPINLIST, file_target_path = "target"):
+    now_path = os.path.dirname(file_source_path).replace('\\', '/')
+    folder = now_path + '/' + file_target_path
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    if file_source_path.endswith('.tstl2'):
+        tstl2_vector(file_source_path, folder, DPINLIST, LCDPINLIST)
+    else:
+        logger.error(f"There is no convert rule for {file_source_path} now, contact author pls")
+
 def header(file_name, fout, channel_list, ch_list_name):
     file_name = file_name.upper()
     file_name = re.sub(r'_', '.', file_name)
-    fout.write("") 
+    fout.write("LPAT    " + file_name + "\n") 
+    fout.write("MODE    QUARTER\n")
+    fout.write("RDX 10\n")
+    fout.write("CHANNEL LCDEXPAND   " + channel_list + "\n")
+    fout.write("CFPF\n")
+    fout.write("LOC 0\n")
+    fout.write(';---------------------------------------------------------------------------------------\n')
+    max_length = len(max(ch_list_name, key = len))
+    tmparr = [""] * max_length
+    for i in range(max_length):
+        for j in range(0, len(ch_list_name)):
+            if i < len(ch_list_name[j]):
+                tmparr[i] = tmparr[i] + ch_list_name[j][i]
+            else:
+                tmparr[i] = tmparr[i] + " "
+        fout.write("\;{:<29} {}\n" .format(" ", tmparr[i]))
+    fout.write(';---------------------------------------------------------------------------------------\n')
+
+def socket_input(file_source_path = Socket.tdl):
+    file_source_path = file_source_path.replace('\\', '/')
+    DPINLIST = defaultdict(list)
+    LCDPINLIST = defaultdict(list)
+    try:
+        with open(file_source_path, 'r', encoding = 'utf-8') as f: #大檔案儲存
+            for line in f:
+                match = re.search(r'(\w+)\s*\.define\s*(.*?)\)', line)
+                if match:
+                    dpin    = match.group(1)
+                    channel = match.group(2)
+                    channel = channel.split(",")
+                    channel = [re.sub(r'\D', '', item) for item in channel]
+                if re.search(r'\bDPIN\b', line):
+                    if dpin in DPINLIST:
+                        logger.warning(f"There is duplicate DPIN {dpin} in DPIN define")
+                        # should add warning log
+                    else:
+                        DPINLIST[dpin] = channel
+                if re.search(r'\bLCDPIN\b', line):
+                    if dpin in LCDPINLIST:
+                        logger.warning(f"There is duplicate LCDPIN {dpin} in LCDPIN define")
+                        # should add warning log
+                    else:
+                        LCDPINLIST[dpin] = channel
+            f.close()
+    except:
+        logger.error(f"There is no Socket.tdl, now path:{file_source_path}")
+    return DPINLIST, LCDPINLIST
+
+#################### main program ####################
+if __name__ == '__main__':
+    logger = setup_logger(name = "CvtModule", log_file = "app.log", level = "DEBUG")
+
+    #script_dir = os.path.dirname(os.path.abspath(__file__))
+    script_dir = os.path.dirname(os.path.realpath(sys.arvg[0])) # 修正打包後的路徑問題
+    source_dir = script_dir + '/pattern/'
+    target_dir = script_dir + '/pattern/target/'
+
+    DPINLIST, LCDPINLIST = socket_input(script_dir + '/Socket.tdl')
+
+    all_files = []
+    for root, dirs, files in os.walk(source_dir):
+        for file in files:
+            # 把完整的檔案路徑加入 list
+            if not (file.endswith('.tdl') or file.endswith('.asc')):
+                all_files.append(os.path.join(root, file))
+    for file_path in all_files:
+        logger.info(f"Processing file:{file_path}")
+        print(f"Processing file:{file_path}")
+        vector_trans(file_path, DPINLIST, LCDPINLIST)
